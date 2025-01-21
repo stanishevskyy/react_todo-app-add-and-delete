4b28eff4da17cb33848d54c5f463f5a4ef16cf96
Your_Name Your_Surname <your@email>
2025-01-21 17:47:41 +0200

    add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { UserWarning } from './UserWarning';
+import { getTodos, USER_ID } from './api/todos';
 
-const USER_ID = 0;
+import { Todo } from './types/Todo';
+import { FilterType } from './types/FilterType';
+import { ErrorType } from './types/ErrorType';
+
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState<ErrorType>(
+    ErrorType.ERROR_DEFAULT,
+  );
+  const [filterTodoBy, setFilterTodoBy] = useState<FilterType>(FilterType.ALL);
+
+  useEffect(() => {
+    const asyncFetch = async () => {
+      try {
+        const loadTodos = await getTodos();
+
+        setTodos(loadTodos);
+      } catch (error) {
+        setErrorMessage(ErrorType.ERROR_LOADING);
+        throw error;
+      }
+    };
+
+    asyncFetch();
+  }, []);
+
+  const filteredTodos = useMemo(() => {
+    return todos.filter(todo => {
+      if (FilterType.ACTIVE === filterTodoBy) {
+        return !todo.completed;
+      }
+
+      if (FilterType.COMPLETED === filterTodoBy) {
+        return todo.completed;
+      }
+
+      return true;
+    });
+  }, [todos, filterTodoBy]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header />
+
+        <TodoList filteredTodos={filteredTodos} />
+
+        {/* Hide the footer if there are no todos */}
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            sortTodoBy={filterTodoBy}
+            onClick={setFilterTodoBy}
+          />
+        )}
+      </div>
+
+      {/* DON'T use conditional rendering to hide the notification */}
+      {/* Add the 'hidden' class to hide the message smoothly */}
+      <ErrorNotification
+        errorMessageTodo={errorMessage}
+        setError={setErrorMessage}
+      />
+    </div>
   );
 };
